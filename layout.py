4d24c16cf88f48bc740022eb0e040ef003df0eca
PIRES014 <everton.reis.pires@escola.pr.gov.br>
2026-06-08 11:51:58 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -1,1 +1,19 @@
-class setorSupermercado
+class SertorSupermercado:
+    def _init_(self,nome_setor,capacidade):
+        self._nome_setor = nome_setor
+        self._capacidade = capacidade
+        self._produtos = []
+
+        # Propiedades
+        @property
+        def nome_setor(self):
+            return self._nome_setor
+        
+        @property
+        def capacidade(self):
+            return self._capacidade
+        
+        # Método Público
+
+
+
